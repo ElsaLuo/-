76cc2cd0fce54751e75ddfe68f061abf3848954f
Elsa Luo <luoying1a@163.com>
2017-11-03 10:05:57 +0800

    new

---
+++ 我的坚果云/研究生搞学术/Docear/mymind.mm
@@ -0,0 +1,99 @@
+<map version="docear 1.1" dcr_id="1509622875105_enzyrs6fvr4r1cn8kthy2hzkj" project="15F511C752D9HM3ZM49U4W8TH30UAYCYQLSN" project_last_home="file:/media/luo/&#x8d44;&#x6599;/&#x6211;&#x7684;&#x575a;&#x679c;&#x4e91;/&#x7814;&#x7a76;&#x751f;&#x641e;&#x5b66;&#x672f;/&#x5927;&#x8bba;&#x6587;/">
+<!--To view this file, download Docear - The Academic Literature Suite from http://www.docear.org -->
+<node TEXT="&#x5a31;&#x4e50;&#x8425;&#x9500;&#x4fe1;&#x606f;&#x6027;" FOLDED="false" ID="ID_1723255651" CREATED="1283093380553" MODIFIED="1509624964635"><hook NAME="MapStyle">
+    <properties show_note_icons="true"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node">
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right">
+<stylenode LOCALIZED_TEXT="default" MAX_WIDTH="600" COLOR="#000000" STYLE="as_parent">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.note"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="9"/>
+<node TEXT="&#x54c1;&#x724c;&#x8bc6;&#x522b;" POSITION="right" ID="ID_1198812202" CREATED="1509624970118" MODIFIED="1509624988995">
+<edge COLOR="#00ff00"/>
+<node TEXT="" ID="ID_766977645" CREATED="1509624992881" MODIFIED="1509624992881"/>
+<node TEXT="" ID="ID_1463685927" CREATED="1509625101861" MODIFIED="1509625101861"/>
+</node>
+<node TEXT="&#x6d88;&#x8d39;&#x8005;&#x8d2d;&#x4e70;&#x51b3;&#x7b56;" POSITION="right" ID="ID_1187289856" CREATED="1509625027255" MODIFIED="1509625039053">
+<edge COLOR="#00ffff"/>
+<node TEXT="&#x7406;&#x8bba;" ID="ID_738231675" CREATED="1509625125835" MODIFIED="1509625132179">
+<node TEXT="ELM &#x6a21;&#x578b;&#x7406;&#x8bba;" ID="ID_1013827373" CREATED="1509625134806" MODIFIED="1509625142916">
+<node ID="ID_1605106830" CREATED="1509625445899" MODIFIED="1509626049440"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Petty(1981)&#25552;&#20986; ELM &#27169;&#22411;&#29702;&#35770;,&#35813;&#29702;&#35770;&#35748;&#20026;&#28040;&#36153;&#32773;&#22312;&#28040;&#36153;&#36807;&#31243;&#20013;&#20250;&#36890;&#36807;<font color="#ff0000">&#20013;&#24515;&#36335; &#24452;&#21644;&#36793; &#32536;&#36335;&#24452;</font>&#36825;&#20004;&#31181; &#26041;&#24335;&#26469; <font color="#ff0000">&#22788;&#29702;&#20182;&#20204;&#25509;&#35302; &#21040;&#30340;&#19982; &#21697;&#29260;&#30456;&#20851;&#30340;&#20449;&#24687;&#24182;&#30001;&#27492;&#24418;&#25104;&#30340;&#26368;&#32456;&#30340;&#21697;&#29260;&#24577;&#24230;</font>;&#28040;&#36153;&#32773;&#22312;&#23545;&#24191;&#21578;&#25552;&#20379;&#30340;&#20449;&#24687;&#20250;&#29702;&#24615;&#30340;&#36827;&#34892;&#24605;&#32771;&#23601; &#26159; &#23545;&#20449; &#24687; &#36827;&#34892;&#31934;&#32454;&#21152; &#24037; <i><b>&#65288;<font color="#ff0000"><u>&#36825;&#37324;&#26159;&#20449;&#24687;&#21152;&#24037;&#65292;&#25105;&#20889;&#30340;&#26159;&#24863;&#30693;&#65292;&#25152;&#20197;&#19981;&#29992;&#65292;&#20063;&#19981;</u></font><font color="#ff3333">&#20914;&#31361;</font></b></i>&#65311;&#21363;&#37319; &#29992;&#26680;<font color="#000000">&#24515;&#36335;&#24452;&#30340;</font>&#160;&#26041;&#24335;&#22788; &#29702;&#20449;&#24687;&#24182;&#30001;&#27492;&#20135;&#29983;&#26368;&#32456;&#30340;&#24577;&#24230;;&#32780;&#22806;&#22260;&#36335;&#24452;&#26159;&#25351;&#28040;&#36153;&#32773;&#19981;&#20250;&#23545;&#24191;&#21578;&#30456;&#20851;&#30340;&#20449;&#24687;<font color="#ff0000">&#36827;&#34892;&#36923;&#36753;&#25512;&#29702;</font>&#32780;&#20165;&#20165;&#26159;&#26681;&#25454;&#24191;&#21578;&#20013;&#30340;&#34523;&#19997;&#39532;&#36857;&#22914;&#31995;&#32479;&#25512;&#33616;&#12289;&#21487;&#20449;&#24230;&#12289;&#23186;&#20171;&#30340;&#26435;&#23041;&#24615;&#25110;&#32773;&#22312;&#24191;&#21578;&#36807;&#31243;&#20013;&#30452;&#35266;&#30340;&#20307;&#39564;&#31561;&#20316;&#20986;&#30340;&#21453;&#24212;&#65288;&#24072;&#22992;&#26681;&#25454;&#36825;&#20010;&#29702;&#35770;&#25552;&#20986;&#28040;&#36153;&#32773;&#20171;&#20837;&#24230;&#36825;&#19968;&#35843;&#33410;&#21464;&#37327;&#65289;
+    </p>
+    <p>
+      <font color="#0000ff">Petty R E,Cacioppo J T,Schumann D.Central and Peripheral Routes toAdvertising Effectiveness:The Moderating Role of Involvement[J].Journal of Consumer Research,1983,10(2):135-146.</font>
+    </p>
+  </body>
+</html>
+
+</richcontent>
+<hook NAME="accessories/plugins/BlinkingNodeHook.properties"/>
+</node>
+</node>
+<node TEXT="&#x793e;&#x4f1a;&#x9884;&#x671f;&#x7406;&#x8bba;" ID="ID_1820757976" CREATED="1509626543529" MODIFIED="1509626543529"/>
+</node>
+</node>
+<node TEXT="&#x6d88;&#x8d39;&#x8005;&#x5e78;&#x798f;&#x611f;" POSITION="right" ID="ID_1786107549" CREATED="1509625039805" MODIFIED="1509625053612">
+<edge COLOR="#ffff00"/>
+</node>
+<node TEXT="&#x751f;&#x6d3b;&#x8d28;&#x91cf;" POSITION="right" ID="ID_1898767368" CREATED="1509625054126" MODIFIED="1509625059875">
+<edge COLOR="#7c0000"/>
+</node>
+<node TEXT="&#x6700;&#x5927;&#x5316;/&#x6ee1;&#x610f;" POSITION="right" ID="ID_1277630487" CREATED="1509625061328" MODIFIED="1509625073648">
+<edge COLOR="#00007c"/>
+</node>
+<node TEXT="&#x5a31;&#x4e50;&#x8425;&#x9500;&#x548c;&#x54c1;&#x724c;&#x7684;&#x5951;&#x5408;&#x5ea6;" POSITION="right" ID="ID_1577391890" CREATED="1509625074388" MODIFIED="1509625092764">
+<edge COLOR="#007c00"/>
+</node>
+</node>
+</map>
